f0b8b8a2176b25172387be88d6dc07e0fbb39501
Fady Morcos <fadymorcos22@gmail.com>
2022-01-11 10:37:32 -0500

    first commit 3

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { auth } from '../firebase-config'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import Header from "./Header";
-import Dropdown from 'react-bootstrap/Dropdown';
 import { ApptContext } from "./ApptContext";
 
 function Model() {
@@ -33,52 +32,10 @@ function Model() {
           <div className="row">
             <div className="col-lg-12 lef"><h4>Brand?</h4></div>
             
-            <div style={{
-              display: 'block',
-              width: 100,
-              marginBottom: 30
-            }}>
-              <Dropdown>
-                <Dropdown.Toggle >
-                  Open Menu
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                <Dropdown.Item href="#">
-                    <Link to='/repairtype'><h6 onClick={() => setRepairDetails({ ...repairDetails, deviceModel : "Apple"})}>Apple</h6></Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#">
-                    <Link to='/repairtype'><h6 onClick={() => setRepairDetails({ ...repairDetails, deviceModel : "Google"})}>Google</h6></Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#">
-                    <Link to='/repairtype'><h6 onClick={() => setRepairDetails({ ...repairDetails, deviceModel : "Samsung"})}>Samsung</h6></Link>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
+            
 
             <div className="col-lg-12 lef"><h4>Model?</h4></div>
-            <div style={{
-              display: 'block',
-              width: 100,
-              marginBottom: 30
-            }}>
-              <Dropdown>
-                <Dropdown.Toggle >
-                  Open Menu
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#">
-                    <Link to='/service'>Apple</Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#">
-                    Google
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#">
-                    Samsung
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
+            
           </div>
         </div>
       </section>
