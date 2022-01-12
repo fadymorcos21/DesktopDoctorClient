b01eabd249b16e288fcccab25598bb2c29d126a9
Fady Morcos <fadymorcos22@gmail.com>
2022-01-12 10:36:32 -0500

    better nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ function Header() {
     const [loggedin, setloggedin] = useState(false);
 
     onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+        setUser(currentUser);
     });
 
     const logout = async () => {
         await signOut(auth)
-      };
+    };
 
 
     return (
@@ -21,36 +21,42 @@ function Header() {
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-lg navbar-dark ">
                     <a class="navbar-brand" href="">Desktop Doctor</a>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item" href="">
-                            <a>Repairs</a>
-                        </li>
-                        <li class="nav-item" href="">
-                            <a>All Services</a>
-                        </li>
-                        <li class="nav-item" href="">
-                            <a>Location</a>
-                        </li>
-                        <li class="nav-item" href="">
-                            <a>Help</a>
-                        </li>
-                        {user === null
-                        ? <li class="nav-item" href="">
-                            <Link to='/signin'>Log in</Link>
-                        </li>
-                        : <li class="nav-item" href="">
-                            {user?.email}
-                        </li>
-                        }
-                        {user === null ? null : <li class="nav-item"  href="">
-                            <a onClick={logout}>Sign Out</a>
-                        </li>}
-                        
-                        
-                    </ul>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item" href="">
+                                <a>Repairs</a>
+                            </li>
+                            <li class="nav-item" href="">
+                                <a>All Services</a>
+                            </li>
+                            <li class="nav-item" href="">
+                                <a>Location</a>
+                            </li>
+                            <li class="nav-item" href="">
+                                <a>Help</a>
+                            </li>
+                            {user === null
+                                ? <li class="nav-item" href="">
+                                    <Link to='/signin'>Log in</Link>
+                                </li>
+                                : <li class="nav-item" href="">
+                                    {user?.email}
+                                </li>
+                            }
+                            {user === null ? null : <li class="nav-item" href="">
+                                <a onClick={logout}>Sign Out</a>
+                            </li>}
+
+
+                        </ul>
+                    </div>
                 </nav>
             </div>
-            </section>
+        </section>
     )
 }
 
