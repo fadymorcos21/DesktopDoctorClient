a94ec9905304141d97fe4f0b8cc76afef80f3021
Fady Morcos <fadymorcos22@gmail.com>
2022-01-16 13:21:28 -0500

    site width fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02" style={{width: '30%'}}>
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item" href="">
                             <Link to="/myrepairs"><a>My Repairs</a></Link>
